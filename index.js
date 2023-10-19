a47164d31b4b77e25d1696ee80d90fe5a320cacc
johnkuefler <john@dev-squared.com>
2023-10-19 08:18:37 -0500

    Moved over form code

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-// run `node index.js` in the terminal
+const express = require('express');
+const path = require('path');
+const app = express();
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/submit', (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  res.send(`Server received: Username - ${username}, Password - ${password}`);
+});
+
+app.post('/calculate', (req, res) => {
+  let firstNumber = Number(req.body.firstNumber);
+  let secondNumber = Number(req.body.secondNumber);
+  let result = firstNumber + secondNumber;
+  res.send(`Result is - ${result}`);
+});
+
+app.get('/test', (req, res) => {
+  res.send('hi there');
+});
+
+app.listen(3000);
